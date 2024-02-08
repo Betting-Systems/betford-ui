22c6879c2e2ade0170bf9265658cabe16965923a
brianmula <mulati.brian@eclectics.io>
2024-02-09 01:45:49 +0300

    added other routes

--- src/features/home/HomeLayout.tsx
+++ src/features/home/HomeLayout.tsx
@@ -1,6 +1,13 @@
 
 export default function HomeLayout() {
   return (
-    <div>HomeLayout</div>
+    <div className="flex">
+      <div className="w-3/4 bg-gray-800">
+      content
+      </div>
+      <div className="w-1/4 bg-gray-900">
+      sidebar
+      </div>
+    </div>
   )
 }

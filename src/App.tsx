b45d15a1fbd90b25b3981fa4f32f2e5d0630bb85
brianmula <mulati.brian@eclectics.io>
2024-02-09 01:45:26 +0300

    added other routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BundesLiga from "./pages/BundesLiga";
 import Home from "./pages/Home";
+import LaLiga from "./pages/LaLiga";
+import League1 from "./pages/League1";
+import PremierLeague from "./pages/PremierLeague";
+import SerieA from "./pages/SerieA";
+import UefaChampionsLeague from "./pages/UefaChampionsLeague";
 import AppLayout from "./ui/AppLayout";
 
 export default function App() {
@@ -8,6 +14,12 @@ export default function App() {
     <Routes>
       <Route element={<AppLayout/>} >
       <Route path="/" element={<Home />} />
+      <Route path="premier-league" element={<PremierLeague/>} />
+      <Route path="la-liga" element={<LaLiga/>} />
+      <Route path="bundesliga" element={<BundesLiga/>} />
+      <Route path="serie-a" element={<SerieA/>} />
+      <Route path="league-1" element={<League1/>} />
+      <Route path="uefa-champions-league" element={<UefaChampionsLeague/>} />
       </Route>
      
       

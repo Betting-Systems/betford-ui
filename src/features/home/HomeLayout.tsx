d26aa1f5ebc4515423cadc5b4c3b56b8af4b4229
brianmula <mulati.brian@eclectics.io>
2024-02-12 01:02:59 +0300

    completed right bar

--- src/features/home/HomeLayout.tsx
+++ src/features/home/HomeLayout.tsx
@@ -1,12 +1,78 @@
+import BetSlip from "./BetSlip"
+import TotalBet from "./TotalBet"
 
 export default function HomeLayout() {
+  const congrats =[
+    {
+      id:1,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:2,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:3,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:4,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:5,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:6,
+      time:'2:51',
+      win:21000
+    },
+    {
+      id:7,
+      time:'2:51',
+      win:21000
+    },
+    
+   
+   
+  ]
   return (
-    <div className="flex">
-      <div className="w-3/4 bg-gray-800">
-      content
+    <div className="block md:flex md:items-start">
+      <div className=" w-full block md:flex md:w-2/3 bg-gray-800 md:items-start">
+        <div className="w-3/4 px-2">
+          <h1 className="px-2 text-lg">Today's Promotion</h1>
+        <div className=" h-36">
+          
+          <img src="https://cdn.pixabay.com/photo/2016/05/20/21/57/football-1406106_640.jpg" alt="" className="h-full w-full rounded-md object-cover" />
+        </div>
+        </div>
+        <div className="w-72 px-2 h-36 overflow-y-auto">
+         <h1 className="px-2 text-lg">Today's Winners</h1>
+         <ul>
+         {
+          congrats.map((congrat, index) => (
+            <li key={index} className="flex">
+            <p className="text-xs">Congradulations Today2:51 <span>Win 21000</span></p>
+            
+          </li>
+          ))
+         }
+         </ul>
+        </div>
       </div>
-      <div className="w-1/4 bg-gray-900">
-      sidebar
+      <div className="w-1/3 bg-gray-900 h-[calc(100vh-6rem)] overflow-y-auto">
+     <div className="h-2/3 overflow-y-auto">
+     <BetSlip/>
+     </div>
+     <div className="h-1/3">
+     <TotalBet/>
+     </div>
       </div>
     </div>
   )

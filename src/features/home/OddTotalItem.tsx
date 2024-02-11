f0f5c6cb1e4b23ee40039f0aca1baa43e2e239ec
brianmula <mulati.brian@eclectics.io>
2024-02-12 01:03:13 +0300

    completed right bar

---
+++ src/features/home/OddTotalItem.tsx
@@ -0,0 +1,8 @@
+
+export default function OddTotalItem() {
+  return (
+    <div className="bg-gray-500 w-24 flex items-center justify-center text-white font-medium text-xs rounded-md py-1">
+        <p>+10</p>
+    </div>
+  )
+}
